d0ee9a7e07434d23636a8479476eb221a03f2b66
tiagoSilvadeAssis <109537384+tiagoSilvadeAssis@users.noreply.github.com>
2022-08-02 02:49:12 -0300

    Lista duplamente Encadeada
    Criação do projeto java de lista duplamente encadeada, implementação de métodos de inserção, busca, isEmpty, e menu de opções

---
+++ DoublyLinkedList/src/DoublyLinkedList.java
@@ -0,0 +1,100 @@
+public class DoublyLinkedList {
+    private int size;
+    private Node head, tail, cursor;
+
+    public DoublyLinkedList() {
+        this.size = 0;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Node getHead() {
+        return this.head;
+    }
+
+    public void setHead(Node head) {
+        this.head = head;
+    }
+
+    public Node getTail() {
+        return this.tail;
+    }
+
+    public void setTail(Node tail) {
+        this.tail = tail;
+    }
+
+    public void add(int value){
+        Node newNode = new Node(value);
+        if(size == 0){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            newNode.setNext(head);
+            head.setPrevious(newNode);
+            head = newNode;
+        }
+        size++;
+    }
+    public void addLast(int value){
+        Node newNode = new Node(value);
+        if(size == 0){
+            head = newNode;
+            tail = newNode;
+        }
+        else{
+            newNode.setPrevious(newNode);
+            tail.setNext(newNode);
+            tail = newNode;
+        }
+        size++;
+    }
+    public void remove(){
+
+    }
+    public void search(int value){
+        cursor = head;
+        for(int i = 0; i < size; i++){
+            if(cursor.getValue() == value){
+                System.out.println("Value " + value + " found.");
+            }
+            advance();
+            return;
+        }
+        System.out.println("Value " + value + " not found.");
+    }
+    public boolean isEmpty(){
+        if(size == 0)
+            return true;
+        else
+            return false;
+    }
+    @Override
+    public String toString(){
+        cursor = head;
+        for(int i = 0; i < size; i++){
+            if(i == 0)
+                System.out.println("head : " + cursor.getValue());
+            else{
+                if((i == (size - 1))){
+                    System.out.println("tail : " + cursor.getValue());
+                    return "";
+                }
+                System.out.println("value: " + cursor.getValue());
+            }
+            advance();
+        }
+        return "";
+    }
+    private void advance(){
+        cursor = cursor.getNext();
+    }
+
+}
---
+++ DoublyLinkedList/src/Main.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        DoublyLinkedList list = new DoublyLinkedList();
+        int op, value;
+
+        do{
+            System.out.print("\n1 - Add\n" +
+                           "2 - remove\n" +
+                           "3 - toString\n" +
+                           "4 - search\n" +
+                           "5 - isEmpty\n" + 
+                           "6 - Order\n" + 
+                           "7 - addLast\n-> ");
+            op = sc.nextInt();
+            System.out.println("\n"); // espaçamento
+            switch(op){
+                case 1:
+                    System.out.print("Enter with a number: ");
+                    value = sc.nextInt();
+                    list.add(value);
+                    break;
+
+                case 2:
+                    if(list.isEmpty()){
+                        System.out.println("The list is empty.");
+                        break;
+                    }
+                    System.out.print("What number you want to remove: ");
+                    value = sc.nextInt();
+                    list.remove();
+                    break;
+
+                case 3:
+                    if(list.isEmpty()){
+                        System.out.println("The list is empty.");
+                        break;
+                    }
+                    list.toString();
+                    break;
+
+                case 4:
+                    if(list.isEmpty()){
+                        System.out.println("The list is empty.");
+                        break;
+                    }
+                    System.out.print("What number you want to search: ");
+                    value = sc.nextInt();
+                    list.search(value);
+                    break;
+
+                case 5:
+                    if(list.isEmpty())
+                        System.out.println("The list is empty.");
+                    else
+                        System.out.println("The list isn't empty.");
+                    break;
+
+                case 6:
+                    if(list.isEmpty()){
+                        System.out.println("The list is empty.");
+                        break;
+                    }
+                    break;
+
+                case 7:
+                    System.out.print("Enter with a number: ");
+                    value = sc.nextInt();
+                    list.addLast(value);
+                    break;
+
+                case 0:
+                    break;
+
+                default:
+                    System.out.println("Invalid option.");
+
+            }
+        }while(!(op == 0));
+    }
+}
